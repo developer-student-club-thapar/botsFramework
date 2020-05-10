da9fbe2cb36e390e773585daf3e1150da20ddddf
Saurav <sauravsrivastav103@gmail.com>
2020-05-10 18:59:35 +0530

    Improved scraper, updated goals.md for the same

--- Medium/goals.md
+++ Medium/goals.md
@@ -9,7 +9,7 @@ This particular project has been further divided into smaller milestones!
 - [x] Build a basic bot to get you articles based on a particular search criteria
 - [x] Add more interactivity in the form of Invalid commands
 - [x] Add a message about what the bot can do
-- [ ] Return more details in the form of an object
+- [x] Return more details in the form of an object
 - [ ] Design a message block with the BlockKit Builder of Slack
 - [ ] Apply the Block
 - [ ] Allow channels to Subscribe to a particular tag and make the bot post the articles on that tag regularly
--- Medium/scraping.py
+++ Medium/scraping.py
@@ -5,21 +5,75 @@ import requests
 def tag_search_scrape(tag):
     source = requests.get(f"https://www.medium.com/tag/{tag}").text
     soup = BeautifulSoup(source, "lxml")
-    links = []
-    for part in soup.findAll("div", class_="postArticle-readMore"):
-        links.append(part.a["href"])
-    return links
+
+    data = []
+    id = 1
+    iter = 0
+
+    for content, author_con, part in zip(
+        soup.findAll("div", class_="section-content"),
+        soup.findAll("div", class_="postMetaInline-authorLockup"),
+        soup.findAll("div", class_="postArticle-readMore"),
+    ):
+        if iter == 3:
+            break
+        title = content.find("h3").text
+        img = content.find("img")["src"]
+        link = part.a["href"]
+        author = author_con.find("a", class_="ds-link").text
+        read_time = author_con.find("span", class_="readingTime")["title"]
+
+        data.append(
+            {
+                "id": id,
+                "title": title,
+                "img": img,
+                "link": link,
+                "author": author,
+                "read_time": read_time,
+            }
+        )
+
+        id += 1
+
+    return data
 
 
 def search_scrape(search):
     source = requests.get(f"https://www.medium.com/search?q={search}").text
     soup = BeautifulSoup(source, "lxml")
-    i = 0
-    link = []
-    for part in soup.findAll("div", class_="postArticle-readMore"):
-        if i == 3:  # Number of articles that need to be fetched
-            break
-        link.append(part.a["href"])
-        # Post to channel
-        i = i + 1
-    return link
+
+    data = []
+    id = 1
+
+    for content, author_con, part in zip(
+        soup.findAll("div", class_="section-content"),
+        soup.findAll("div", class_="postMetaInline-authorLockup"),
+        soup.findAll("div", class_="postArticle-readMore"),
+    ):
+        try:
+            title = content.find("h3", class_="graf").text
+        except Exception:
+            title = content.find("h2", class_="graf").text
+        try:
+            img = content.find("img")["src"]
+
+        except Exception:
+            img = None
+        link = part.a["href"]
+        author = author_con.find("a", class_="ds-link").text
+        read_time = author_con.find("span", class_="readingTime")["title"]
+
+        data.append(
+            {
+                "id": id,
+                "title": title,
+                "img": img,
+                "link": link,
+                "author": author,
+                "read_time": read_time,
+            }
+        )
+        id += 1
+
+    return data
