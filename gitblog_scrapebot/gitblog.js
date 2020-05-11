3ce252328b1e17e543fe4e7e56daf7f1c0135aba
specter25 <ujjwalcoding012@gmail.com>
2020-05-11 16:45:06 +0530

    rendering enhanced

--- gitblog_scrapebot/gitblog.js
+++ gitblog_scrapebot/gitblog.js
@@ -20,10 +20,15 @@ const fetchblogs=async(url)=>{
  return rp(options)
   .then(($) => {
     // console.log($);
+    const blocks = [];
+    const divider=	{
+        "type": "divider"
+    };
     $('.post-item').each(function(index)
                 {
                     const currentdate=Date.now()
                     const date = $(this).find('.post-item__date').find('time').attr('datetime');
+                    const datestr = $(this).find('.post-item__date').find('time').text().trim();
                     const pheading=$(this).find('.post-item__title').find('a').text();
                     const plink=$(this).find('.post-item__title').find('a').attr('href');
                     const pdesc=$(this).find('.post-item__excerpt').find('p').text();
@@ -36,9 +41,33 @@ const fetchblogs=async(url)=>{
                         // console.log(`description :${pdesc}`);
                         // console.log('------------------------------------------------------',);
                     // }
+                    const block={
+                      "type": "section",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": `*<${plink}|${pheading.trim()}>* \n ${pdesc}`
+                      },
+                      "accessory": {
+                        "type": "image",
+                        "image_url": "http://pngimg.com/uploads/github/github_PNG40.png",
+                        "alt_text": "alt text for image"
+                      }
+                    }
+                    const context2=		{
+                      "type": "context",
+                      "elements": [
+                        {
+                          "type": "mrkdwn",
+                          "text": `*Date:* ${datestr}`
+                        }
+                      ]
+                    }
+                    blocks.push(block);
+                    blocks.push(context2);
+                    blocks.push(divider);
                     
                 });
-                return final;
+                return blocks;
 
   })
   .catch((err) => {
