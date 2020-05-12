f39aed4d4e745981286066e7f34e964ca5bae337
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-12 11:08:07 +0530

    testing changes

--- Telegram/Slack_Intigrater.py
+++ Telegram/Slack_Intigrater.py
@@ -4,9 +4,11 @@ from cred import *
 slack = Slacker(Slack_token)
 
 def serve_slack(message):
-    slack.chat.post_message(Slack_Channel, message)
+    slack.chat.post_message(Slack_Channel1, message)
+    slack.chat.post_message(Slack_Channel1, "/poll Is this a hackathon link?\" \"Sure\" \"Hell No!")
     slack.chat.command(
-        channel=Slack_Channel_ID,
+        channel=Slack_Channel_ID1,
         command="/poll",
         text="Is this a hackathon link?" "Sure" "Hell No!"
         )
+serve_slack('hi')
