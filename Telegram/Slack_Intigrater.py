5d75af58b56dc55d5e4ac86115b784075816a102
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-12 13:57:47 +0530

    added innitial thubs up and thumbs down

--- Telegram/Slack_Intigrater.py
+++ Telegram/Slack_Intigrater.py
@@ -1,14 +1,19 @@
 from slacker import Slacker
+import decimal
 from cred import *
 
 slack = Slacker(Slack_token)
 
 def serve_slack(message):
-    slack.chat.post_message(Slack_Channel1, message)
+    obj=slack.chat.post_message(Slack_Channel1, message)
+    slack.reactions.add("+1" , channel=obj.__dict__['body']['channel'], timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
+    slack.reactions.add("-1" , channel=obj.__dict__['body']['channel'], timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
+    
 ##    slack.chat.post_message(Slack_Channel1, "/poll Is this a hackathon link?\" \"Sure\" \"Hell No!")
 ##    slack.chat.command(
 ##        channel=Slack_Channel_ID1,
 ##        command="/poll",
 ##        text="Is this a hackathon link?" "Sure" "Hell No!"
 ##        )
-##serve_slack('hi')
+##serve_slack('Test 182')
+        
