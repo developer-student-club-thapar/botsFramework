ca06e976a98265ad5c99a02865bf3763879a3ef5
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-12 14:49:45 +0530

    read reactions (raw), need to parse

--- Telegram/Slack_Intigrater.py
+++ Telegram/Slack_Intigrater.py
@@ -3,17 +3,24 @@ import decimal
 from cred import *
 
 slack = Slacker(Slack_token)
+metadata=[]
 
 def serve_slack(message):
     obj=slack.chat.post_message(Slack_Channel1, message)
-    slack.reactions.add("+1" , channel=obj.__dict__['body']['channel'], timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
-    slack.reactions.add("-1" , channel=obj.__dict__['body']['channel'], timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
+    slack.reactions.add("+1" , channel=obj.__dict__['body']['channel'],
+                        timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
+    slack.reactions.add("-1" , channel=obj.__dict__['body']['channel'],
+                        timestamp=decimal.Decimal(obj.__dict__['body']['ts']))
+    global metadata
+    metadata.append([decimal.Decimal(obj.__dict__['body']['ts']), obj.__dict__['body']['channel']])
+
+
+
+def reaction_feedback(data):
+    print(slack.reactions.get(timestamp=data[0], channel=data[1]))
+
     
-##    slack.chat.post_message(Slack_Channel1, "/poll Is this a hackathon link?\" \"Sure\" \"Hell No!")
-##    slack.chat.command(
-##        channel=Slack_Channel_ID1,
-##        command="/poll",
-##        text="Is this a hackathon link?" "Sure" "Hell No!"
-##        )
+    
 ##serve_slack('Test 182')
-        
+##print(metadata)
+##reaction_feedback(metadata)
