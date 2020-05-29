304a7c1511920b100226a91fa6ebb24d29a70f31
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-30 04:07:54 +0530

    minor changes

--- Telegram/main.py
+++ Telegram/main.py
@@ -5,7 +5,7 @@ from database import *
 contain_url(get_updates())             ## From Scrapper.py to get all mesages that contain url
 remove_dublicate(message)              ## Removes Dublicate Messages (Due to the message containing multiple url)
 
-Add_to_Sheets(message)                      ## Adds the message and metadata to google sheet
+Add_to_Sheets(message)                 ## Adds the message and metadata to google sheet
 
 for i in message:                      ## Serves message to slack | Possible incorporate loop into serve_slack
     serve_slack(i[0])
